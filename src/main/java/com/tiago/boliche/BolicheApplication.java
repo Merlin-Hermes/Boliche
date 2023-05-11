18ee571b69c281ada66510ad6ec60b910c0a138b
tiago.ferreira <tiago.ferreira@crptecnologia.com.br>
2023-05-11 16:59:40 -0300

    Criação do projeto.

---
+++ src/main/java/com/tiago/boliche/BolicheApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.boliche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolicheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolicheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiago/boliche/BolicheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.boliche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolicheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
